e4df4eed17c008f98d3049966e652f4d33c08c16
José Luis González <joseluisgonsan@gmail.com>
2022-06-22 09:06:46 +0200

    Localización

---
+++ web2/backup/vuepress-theme-reco/locales/es.js
@@ -0,0 +1,9 @@
+export default {
+  all: 'Todas',
+  article: 'Artículos',
+  tag: 'Etiquetas',
+  category: 'Categorías',
+  friendLink: 'Páginas amigas',
+  timeLine: 'Cronología',
+  timeLineMsg: 'Regreso al futuro, o al pasado 🚀'
+}
