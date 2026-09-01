d14e3ccb565224488e1de9aff08074dfa3ad53a6
José Luis González <joseluisgonsan@gmail.com>
2026-09-01 20:41:52 +0200

    revert: volver configuracion original del feed y workflow blog-post

--- web/src/.vuepress/theme.ts
+++ web/src/.vuepress/theme.ts
@@ -197,8 +197,8 @@ export default hopeTheme({
       hostname: 'https://joseluisgs.dev',
       author: {
         name: 'José Luis González Sánchez',
+        email: 'joseluis@joseluisgs.dev',
       },
-      filter: (page) => !page.path.includes('404'),
     },
 
     sitemap: {
